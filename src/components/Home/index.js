98e89db31507332cc9c20fbbecfd395e67f11a6b
kenjebek0vva <ainaz20042602@gmail.com>
2023-04-20 14:05:18 +0600

    first project

---
+++ src/components/Home/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './style.scss'
+import NewBooks from "../HewBooks";
+
+const Home = () => {
+    return (
+        <div>
+            <NewBooks/>
+        </div>
+    );
+};
+
+export default Home;
